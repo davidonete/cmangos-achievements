636a93bf3c329a620708719943c309955d994a1d
David <davidparraausina@gmail.com>
2024-01-26 22:16:39 +0000

    Move includes to pch

--- src/AchievementScriptMgr.cpp
+++ src/AchievementScriptMgr.cpp
@@ -1,38 +1,4 @@
-/*
- * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
-// #include "Chat.h"
-// #include "Config.h"
-// #include "CreatureAI.h"
-// #include "DatabaseEnv.h"
-//#include "DBCStores.h"
-// #include "GossipDef.h"
-// #include "ObjectMgr.h"
-// #include "OutdoorPvPMgr.h"
-// #include "Player.h"
-// #include "ScriptedGossip.h"
-#include "AchievementScriptMgr.h"
-// #include "ScriptSystem.h"
-// #include "SpellInfo.h"
-// #include "SpellScript.h"
-// #include "Transport.h"
-// #include "Vehicle.h"
-// #include "SmartAI.h"
-// #include "WorldPacket.h"
+#include "achievementspch.h"
 
 INSTANTIATE_SINGLETON_1(AchievementScriptMgr);
 
--- src/achievement_scripts.cpp
+++ src/achievement_scripts.cpp
@@ -1,34 +1,7 @@
-/*
- * This file is part of the AzerothCore Project. See AUTHORS file for Copyright information
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation; either version 3 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+#include "achievementspch.h"
 
-#include "BattleGround.h"
-#include "BattleGroundAB.h"
-#include "BattleGroundAV.h"
-// // #include "BattleGroundIC.h"
-// // #include "BattleGroundSA.h"
-#include "BattleGroundWS.h"
-// #include "Creature.h"
-// #include "Player.h"
-#include "Achievements/AchievementScriptMgr.h"
-// #include "Vehicle.h"
-
-//using namespace Achievement;
-
-class achievement_resilient_victory: public AchievementCriteriaScript {
+class achievement_resilient_victory: public AchievementCriteriaScript 
+{
 public:
     achievement_resilient_victory() : AchievementCriteriaScript("achievement_resilient_victory") { }
 
